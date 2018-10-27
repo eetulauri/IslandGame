8c7607282b539e67bb4556b6429a013006b6897a
Eetu Pulkkinen <eetu.pulkkinen@student.tut.fi>
2018-10-27 16:40:32 +0300

    projekti alustettu ja mainwindow ikkuna luotu

---
+++ UI/mainwindow.hh
@@ -0,0 +1,18 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+
+};
+
+#endif // MAINWINDOW_HH
