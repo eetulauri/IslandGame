7dd03dfe0acd4e34defea7aee9f86aab26084b65
Eetu Pulkkinen <eetu.pulkkinen@student.tut.fi>
2018-12-03 16:01:14 +0200

    kommentoitu gameboard.hh

--- UI/gameboard.hh
+++ UI/gameboard.hh
@@ -29,28 +29,68 @@ public:
     GameBoard();
     ~GameBoard() override;
 
+    // Documentation in IgameBoard
     int checkTileOccupation(Common::CubeCoordinate tileCoord) const override;
+    // Documentation in IgameBoard
     bool isWaterTile(Common::CubeCoordinate tileCoord) const override;
+    // Documentation in IgameBoard
     std::shared_ptr<Common::Hex> getHex(Common::CubeCoordinate hexCoord) const override;
+    // Documentation in IgameBoard
     void addPawn(int playerId, int pawnId) override;
+    // Documentation in IgameBoard
     void addPawn(int playerId, int pawnId, Common::CubeCoordinate coord) override;
+    // Documentation in IgameBoard
     void movePawn(int pawnId, Common::CubeCoordinate pawnCoord) override;
+    // Documentation in IgameBoard
     void removePawn(int pawnId) override;
     void addActor(std::shared_ptr<Common::Actor> actor, Common::CubeCoordinate actorCoord) override;
+    // Documentation in IgameBoard
     void moveActor(int actorId, Common::CubeCoordinate actorCoord) override;
+    // Documentation in IgameBoard
     void removeActor(int actorId) override;
-
+    // Documentation in IgameBoard
     void addHex(std::shared_ptr<Common::Hex> newHex) override;
+    // Documentation in IgameBoard
     void addTransport(std::shared_ptr<Common::Transport> transport, Common::CubeCoordinate coord) override;
+    // Documentation in IgameBoard
     void moveTransport(int id, Common::CubeCoordinate coord) override;
+    // Documentation in IgameBoard
     void removeTransport(int id) override;
 
+    /**
+     * @brief createPawns creates pawns to their designated corners
+     */
+
     void createPawns();
+    /**
+     * @brief addPlayer stores player object into players_
+     * @param player shared_ptr to player
+     */
     void addPlayer(std::shared_ptr<Student::Player> player);
+
+    /**
+     * @brief getPlayerVector
+     * @return vector of player objects
+     */
     std::vector<std::shared_ptr<Student::Player> > getPlayerVector();
 
+    /**
+     * @brief calculateCornerPieces calculates the corners of the gameboard
+     * and stores them in to a vector
+     * @return vector to all the 6 possible corner pieces
+     */
     std::vector<Common::CubeCoordinate> calculateCornerPieces();
+
+    /**
+     * @brief getHexesContainer
+     * @return a map containing coordinate and corresponding hex
+     */
     std::map<Common::CubeCoordinate, std::shared_ptr<Common::Hex>> getHexesContainer();
+
+    /**
+     * @brief getPawnMap
+     * @return a mapw containing pawns id and pawn shared_ptr to pawn object
+     */
     std::unordered_map<int, std::shared_ptr<Common::Pawn>> getPawnMap();
 
 
